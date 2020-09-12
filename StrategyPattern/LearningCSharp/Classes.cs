6bbb0bfbf5eff6a1d77922b5073fa639de12a5c4
= <ljaroszewski@kbmax.com>
2020-09-12 14:52:09 -0500

    StrategyPattern

---
+++ StrategyPattern/LearningCSharp/Classes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Dog
+{
+    private  SpeakBehavior speakBehavior;
+    public void speak()
+    {
+        this.speakBehavior.speak();
+    }
+    public void setSpeakBehavior(SpeakBehavior sb )
+    {
+        this.speakBehavior = sb; 
+    }
+}
+
+public interface SpeakBehavior
+{
+    public void speak();
+}
+
+public class SpeakLikeDog : SpeakBehavior
+{
+    public void speak()
+    {
+        Console.WriteLine("Ruff");
+    }
+     
+}
+public class SpeakLikeCat : SpeakBehavior
+{
+    public void speak()
+    {
+        Console.WriteLine("Meow");
+    }
+
+}
---
+++ StrategyPattern/LearningCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Dog dog = new Dog(); 
+            dog.setSpeakBehavior(new SpeakLikeDog());
+            dog.speak();
+        }
+    }
+}
